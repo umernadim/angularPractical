1dbd42ed32420c31aa8c3ddaf7ef58e5d697c8f4
umernadim <itsumernadeem@gmail.com>
2025-08-02 11:25:14 +0500

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CarouselComponent } from './carousel/carousel.component';
 import { CardsComponent } from './cards/cards.component';
-import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink,CarouselComponent,CardsComponent,FooterComponent],
+  imports: [RouterOutlet,RouterLink,CarouselComponent,CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
